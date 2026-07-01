e31664574f0ea4ca6fdb0f88a38b3593f314440e
ritesh <riteshpatil8554@gmail.com>
2026-07-01 10:23:19 +0530

    feat(docker): add Docker HEALTHCHECK

--- backend/src/main/java/com/jobx/server/config/SecurityConfig.java
+++ backend/src/main/java/com/jobx/server/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -14,6 +15,7 @@ import org.springframework.security.web.SecurityFilterChain;
 
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 	@Bean
 	UserDetailsService userDetailsService(
@@ -33,16 +35,10 @@ public class SecurityConfig {
 	 @Bean
 	    SecurityFilterChain securityFilterChain(HttpSecurity http)
 	            throws Exception {
-
-	        http
-
-	            .csrf(csrf -> csrf.disable())
-
-	            .authorizeHttpRequests(auth -> auth
-
-	                    .requestMatchers("/api/auth/**").permitAll()
-
-	                    .anyRequest().authenticated()
+	        http.csrf(csrf -> csrf.disable())
+					.authorizeHttpRequests(auth -> auth
+							.requestMatchers("/api/auth/**","/actuator/health/**", "/actuator/info").permitAll()
+							.anyRequest().authenticated()
 
 	            )
 
