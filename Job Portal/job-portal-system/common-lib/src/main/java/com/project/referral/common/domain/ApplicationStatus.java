bbe026292de988979986429316745e61a5484d58
ritesh <riteshpatil8554@gmail.com>
2026-07-29 10:26:43 +0530

    update

---
+++ Job Portal/job-portal-system/common-lib/src/main/java/com/project/referral/common/domain/ApplicationStatus.java
@@ -0,0 +1,11 @@
+package com.project.referral.common.domain;
+
+public enum ApplicationStatus {
+    PENDING,
+    REVIEWING,
+    SHORTLISTED,
+    INTERVIEW_SCHEDULED,
+    REJECTED,
+    HIRED,
+    WITHDRAWN
+}
