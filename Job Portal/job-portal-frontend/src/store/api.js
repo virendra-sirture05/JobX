648b0d70a38ee2c2f322a9e1bd2d1ae325082f86
ritesh <riteshpatil8554@gmail.com>
2026-08-20 10:46:15 +0530

    changed ip address to ec2

--- Job Portal/job-portal-frontend/src/store/api.js
+++ Job Portal/job-portal-frontend/src/store/api.js
@@ -1,7 +1,7 @@
 ﻿import axios from "axios"
 
 // Base URL - Update this with your actual backend URL
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:5000"
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://13.205.248.245:5000"
 
 
 // Create axios instance
